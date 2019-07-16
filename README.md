9eca968b42c26e2b75f8fc699f47396ef54f18c2
John Duimovich <jduimovich@gmail.com>
2019-07-16 13:05:16 +0100

    simple update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bigtrout
+
+Sample simple app.
+Implements /hello
+
+... does a Docker build for container
+... does a Simple Istio Deployment for /hello endpoint
--- app.js
+++ app.js
@@ -1,29 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function(req, res) {
-	console.log("Hello world received a request."); 
+app.get("/hello", function(req, res) {
+	console.log("GET /hello ");
 	const headers = JSON.stringify(req.headers, null, 4);
 	console.log(headers); 
-
-	var target = process.env.TARGET || "Hello CRUEL World";
+	var target = process.env.TARGET || "Hello World";
 	target += "<br><pre>" + headers + "</pre>";
-	res.send("/ " + target + "!");
-	});
-	
-	app.get("/hello", function(req, res) {
-		console.log("Hello world received a request."); 
-		const headers = JSON.stringify(req.headers, null, 4);
-		console.log(headers); 
-	
-		var target = process.env.TARGET || "Hello World";
-		target += "<br><pre>" + headers + "</pre>";
-		res.send("/hello " + target + "!");
-		});
-
+	res.send("/hello " + target + "!");
+	console.log("done"); 
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, function() {
-	console.log("xyz Hello world listening on port", port);
+	console.log("Hello world listening on port", port);
     });
 
