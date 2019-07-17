fd03e20993b28b4127ab88b18bcd8ae5cd72690d
John Duimovich <jduimovich@gmail.com>
2019-07-17 15:33:19 +0100

    new version

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get("/hello", function(req, res) {
 	console.log("GET /hello ");
 	const headers = JSON.stringify(req.headers, null, 4);
 	console.log(headers); 
-	var target = "Hello V10 Changes World";
+	var target = "Hello V11 - push me Changes World";
 	target += "<br><pre>" + headers + "</pre>";
 	res.send("/hello " + target + "!");
 	console.log("done"); 
